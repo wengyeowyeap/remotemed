7ea402b5bd8203113a8a4b8282b1b6eaad815c2a
Ogg <weikhang_93@hotmail.com>
2020-08-24 13:58:59 +0800

    Added LoginForm in homepage

--- src/pages/HomeSignInPage.js
+++ src/pages/HomeSignInPage.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm'
+import { Container } from 'reactstrap';
+
+
+
 
 const HomeSignInPage = () => {
+  document.title="HomePage"
 
-  return 
+  return <>
+
+
+    <Container className='mt-5'>
+
+      <LoginForm></LoginForm>
+
+    </Container>
+
+
+  </>
 
 
 
