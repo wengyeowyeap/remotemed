79820a0848fa44860342f95ffcc9d6107b8805e2
Ogg <weikhang_93@hotmail.com>
2020-08-26 18:34:03 +0800

    fix conflict

--- src/App.js
+++ src/App.js
@@ -6,28 +6,15 @@ import AdminPage from './pages/AdminPage';
 import HomeSignInPage from './pages/HomeSignInPage'
 import DoctorPage from './pages/DoctorPage'
 import Topbar from './components/Topbar';
-<<<<<<< HEAD
 import PatientPage from './pages/PatientPage'
 import Example from "./testing";
-import {ToastContainer} from 'react-toastify'
-=======
 import Footer from './components/Footer'
->>>>>>> 19be9350695a569cf020371c898937dd3570ba28
 
 function App() {
   const [token, setToken] = useState(null)
 
   return (
     <>
-<<<<<<< HEAD
-      <div>
-        <ToastContainer/>
-        <Topbar/>
-        <Switch>
-              <Route path='/testing'>
-                <Example/>
-              </Route>
-=======
       <ToastContainer />
         <div id="page-container">
           <Topbar token={token} setToken={setToken}/>
@@ -37,24 +24,17 @@ function App() {
                 <Route exact path="/">
                   <HomeSignInPage token={token} setToken={setToken}/>
                 </Route>
->>>>>>> 19be9350695a569cf020371c898937dd3570ba28
 
                 <Route path="/admin">
                   <AdminPage/>
                 </Route>
-
-<<<<<<< HEAD
-              <Route path="/admin">
-                <AdminPage/>
-              </Route>
+                
               <Route path="/patient">
                 <PatientPage/>
               </Route>
-=======
                 <Route path="/doctor">
                   <DoctorPage/>
                 </Route>
->>>>>>> 19be9350695a569cf020371c898937dd3570ba28
 
           </Switch>
           </div>
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -11,13 +11,9 @@ import {
 } from 'reactstrap';
 import { NavLink as RouterNavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import LoginModal from './LoginModal'
-=======
 import { useHistory } from 'react-router-dom';
 import { faSignOutAlt} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
->>>>>>> 19be9350695a569cf020371c898937dd3570ba28
 
 const Topbar = (props) => {
   const {token, setToken} = props
@@ -48,11 +44,6 @@ const Topbar = (props) => {
               <NavLink tag={RouterNavLink} to="/">Home</NavLink>
             </NavItem> */}
           </Nav>
-<<<<<<< HEAD
-          <NavbarText className= "mr-3">RemoteMed</NavbarText>
-          <NavLink style={{color:"lavender"}}>
-             <LoginModal/>
-=======
 
           {/* temporary working use */}
           <NavLink tag={RouterNavLink} to="/admin" style={{color:"#205072"}}>
@@ -60,7 +51,6 @@ const Topbar = (props) => {
           </NavLink>
           <NavLink tag={RouterNavLink} to="/doctor" style={{color:"#205072"}}>
               Doctor
->>>>>>> 19be9350695a569cf020371c898937dd3570ba28
           </NavLink>
           {/* end */}
 
